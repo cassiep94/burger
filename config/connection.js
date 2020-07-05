79ae35b4dd21ff87057ad068a1f3c94310e9a8ee
Cassandra Perez <cassandraenriq@icloud.com>
2020-07-04 23:16:55 -0500

    updated host

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,19 @@
-const mysql = require("mysql");
-let connection;
+var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
-        connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-        connection = mysql.createConnection({
-        host: process.env.HOST,
-        user: process.env.USER,
-        port: process.env.PORT, 
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE
-    });
-};
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
 
-connection.connect(function(err){
-    if (err) {
-        console.error("error connection: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+  host: "localhost",
+  //port: 3306,
+  user: "root",
+  password: "Rk0978tb",
+  database: "burgers_db"
 });
-
+};
+// Make connection.
+connection.connect();
+  
 module.exports = connection;
-
